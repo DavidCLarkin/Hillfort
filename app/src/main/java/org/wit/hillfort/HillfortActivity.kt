0a5d0a40b5b265963e5e0dd9b09250d6ba26efe4
David Larkin <davidlarkin96@gmail.com>
2018-10-16 23:58:42 +0100

    Implemented AnkoLogger for later use for debuggung and building the application, verified that it works.

--- app/src/main/java/org/wit/hillfort/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/HillfortActivity.kt
@@ -2,12 +2,15 @@ package org.wit.hillfort
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class HillfortActivity : AppCompatActivity()
+class HillfortActivity : AppCompatActivity(), AnkoLogger
 {
-  override fun onCreate(savedInstanceState: Bundle?)
-  {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_hillfort)
-  }
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort)
+        info("Working")
+    }
 }
