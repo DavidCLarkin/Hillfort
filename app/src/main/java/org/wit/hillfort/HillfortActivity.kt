4ded73e0b5d9d62d751c4ace1b0af7b0a7e154ea
David Larkin <davidlarkin96@gmail.com>
2018-10-17 01:06:58 +0100

    Added a Toolbar

--- app/src/main/java/org/wit/hillfort/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/HillfortActivity.kt
@@ -2,8 +2,12 @@ package org.wit.hillfort
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger
 {
@@ -11,6 +15,36 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        setSupportActionBar(findViewById(R.id.mainToolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         info("Working")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    // actions on click menu items
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_text ->
+        {
+            // User chose the "Print" item
+            Toast.makeText(this,"Print action",Toast.LENGTH_LONG).show()
+            true
+        }
+        android.R.id.home ->
+        {
+            Toast.makeText(this,"Home action",Toast.LENGTH_LONG).show()
+            true
+        }
+
+        else ->
+        {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
     }
 }
