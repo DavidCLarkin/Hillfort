a727b6121aebde52c056f450182173b84271213d
David Larkin <davidlarkin96@gmail.com>
2018-10-25 19:11:00 +0100

    Added a Splash Screen Activity and functionality for a splash screen. Changed the LAUNCHER activity to the be the splash screen, and after a short delay, the app beginds. Later to be updated with a Login page.

---
+++ app/src/main/java/org/wit/hillfort/activities/SplashScreenActivity.kt
@@ -0,0 +1,40 @@
+package org.wit.hillfort.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import org.wit.hillfort.R
+
+class SplashScreenActivity : AppCompatActivity()
+{
+
+    private var delayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 1500
+
+    internal val runnable: Runnable = Runnable {
+        if(!isFinishing)
+        {
+            val intent = Intent(applicationContext, HillfortListActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        delayHandler = Handler()
+        delayHandler!!.postDelayed(runnable, SPLASH_DELAY)
+    }
+
+    public override fun onDestroy()
+    {
+        if(delayHandler != null)
+        {
+            delayHandler!!.removeCallbacks(runnable)
+        }
+        super.onDestroy()
+    }
+}
