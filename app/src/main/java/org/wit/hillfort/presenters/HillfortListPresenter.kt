713281636377c5c74f06454bbfed8ddab861788e
David Larkin <davidlarkin96@gmail.com>
2018-12-05 16:58:30 +0000

    Fixed bugs where view wouldn't update upon adding/creating/deleting a Hillfort. Now functions correctly.

---
+++ app/src/main/java/org/wit/hillfort/presenters/HillfortListPresenter.kt
@@ -0,0 +1,34 @@
+package org.wit.hillfort.models
+
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.startActivityForResult
+import org.wit.hillfort.activities.HillfortActivity
+import org.wit.hillfort.activities.HillfortListActivity
+import org.wit.hillfort.activities.HillfortMapsActivity
+import org.wit.hillfort.main.MainApp
+import org.wit.hillfort.models.HillfortModel
+
+class PlacemarkListPresenter(val activity: HillfortListActivity)
+{
+
+    var app: MainApp
+
+    init {
+        app = activity.application as MainApp
+    }
+
+    fun getPlacemarks() = app.hillforts.findAll()
+
+    fun doAddPlacemark() {
+        activity.startActivityForResult<HillfortActivity>(0)
+    }
+
+    fun doEditPlacemark(placemark: HillfortModel) {
+        activity.startActivityForResult(activity.intentFor<HillfortActivity>().putExtra("placemark_edit", placemark), 0)
+    }
+
+    fun doShowPlacemarksMap() {
+        activity.startActivity<HillfortMapsActivity>()
+    }
+}
